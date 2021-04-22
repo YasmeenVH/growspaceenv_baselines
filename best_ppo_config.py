167e5edd10792104466217c16532d38e870f488f
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-22 23:34:34 +0200

    basleines zoo

---
+++ best_ppo_config.py
@@ -0,0 +1,54 @@
+import torch
+
+import experiment_buddy
+
+FIRST_BRANCH_HEIGHT = .42
+BRANCH_THICCNESS = 0.015
+BRANCH_LENGTH = 1 / 9
+MAX_BRANCHING = 10
+LIGHT_WIDTH = .25
+LIGHT_DIF = 250
+algo = "ppo"
+gail = False
+gail_experts_dir = './gail_experts'
+gail_batch_size = 128
+gail_epoch = 5
+lr = 0.04747
+eps = 0.003255
+alpha = 0.99
+gamma = 0.2597
+use_gae = False
+gae_lambda = 0.3391
+entropy_coef = 0.3466
+value_loss_coef = 0.3693
+max_grad_norm = 0.1471
+seed = 1
+cuda_deterministic = False
+num_processes = 1
+num_steps = 3463
+custom_gym = "growspace"
+ppo_epoch = 15
+num_mini_batch = 46
+clip_param = 0.4327
+log_interval = 10
+save_interval = 100
+eval_interval = None
+num_env_steps = 1e6
+env_name = "GrowSpaceEnv-Control-v0" # "GrowSpaceEnv-Continuous-v0"
+log_dir = "/tmp/gym/"
+save_dir = "./trained_models/"
+use_proper_time_limits = False
+recurrent_policy = False
+use_linear_lr_decay = True
+no_cuda = False
+cuda = not no_cuda and torch.cuda.is_available()
+optimizer = "adam"
+momentum = 0.9  # if sgd is used
+
+experiment_buddy.register(locals())
+tensorboard = experiment_buddy.deploy(
+    "",
+    sweep_yaml="",
+    proc_num=10,
+    wandb_kwargs={"entity": "growspace"}
+)
---
+++ policies_zoo.py
@@ -0,0 +1,61 @@
+import best_ppo_config as config
+
+import os
+
+import gym
+from gym.envs.registration import register
+# from stable_baselines import PPO2
+# from stable_baselines.common.policies import CnnPolicy
+import logging
+import os
+import time
+
+import cv2
+import numpy as np
+import torch
+import torch.backends.cudnn
+import torch.utils.data
+from gym.spaces.discrete import Discrete
+
+import wandb
+
+from stable_baselines3 import PPO
+
+env = gym.make("CartPole-v1")
+
+# model = PPO("MlpPolicy", env, verbose=1)
+# model.learn(total_timesteps=10)
+#
+# obs = env.reset()
+# for i in range(10):
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, reward, done, info = env.step(action)
+#     env.render()
+#     if done:
+#         obs = env.reset()
+#
+# env.close()
+
+register(
+    id=f'GrowSpaceEnv-Continuous-v0',
+    entry_point='growspace.env:GrowSpaceContinuous',
+    max_episode_steps=50,
+)
+
+
+def ppo2_stable_baselines_training():
+    wandb.run = config.tensorboard.run
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
+
+    envs = gym.make(config.env_name)
+
+    model = PPO2(
+        CnnPolicy, envs, verbose=1, tensorboard_log="./ppo-stable-tensorboard-plant/", cliprange=0.2, n_steps=2500
+    )
+    model.learn(total_timesteps=1000000, log_interval=10)
+    model.save("stable_baselines_ppo2_continuous_plant")
+
+
+if __name__ == "__main__":
+    ppo2_stable_baselines_training()
