198d39c0be708acab3ae5f2e8c3d51ff9a74f8f2
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-23 17:54:48 +0200

    [CLUSTER] mnsit_with_heirarchy_best_hps

--- config.py
+++ config.py
@@ -8,33 +8,41 @@ BRANCH_LENGTH = 1 / 9
 MAX_BRANCHING = 10
 LIGHT_WIDTH = .25
 LIGHT_DIF = 250
+
+lr = 0.06348
+eps = 0.03238
+gamma = 0.9805
+use_gae = True
+gae_lambda = 0.7463
+entropy_coef = 0.178
+value_loss_coef = 0.563
+max_grad_norm = 0.3398
+num_steps = 3244
+optimizer= "adam"
+ppo_epoch = 19
+num_mini_batch = 32
+clip_param = 0.08664
+use_linear_lr_decay = False
+
+
 algo = "ppo"
 gail = False
 gail_experts_dir = './gail_experts'
 gail_batch_size = 128
 gail_epoch = 5
-lr = 2.5e-4
-eps = 1e-5
 alpha = 0.99
-gamma = 0.99
-use_gae = True
-gae_lambda = 0.95
-entropy_coef = 0.01
-value_loss_coef = 0.5
-max_grad_norm = 0.5
 seed = 1
 cuda_deterministic = False
 num_processes = 1
-num_steps = 2500
 custom_gym = "growspace"
-ppo_epoch = 4
-num_mini_batch = 32
-clip_param = 0.1
 log_interval = 10
 save_interval = 100
 eval_interval = None
 num_env_steps = 1e6
-env_name = "GrowSpaceEnv-Control-v0" # "GrowSpaceEnv-Continuous-v0"
+# env_name = "GrowSpaceEnv-Control-v0"
+# env_name = "GrowSpaceEnv-Hierarchy-v0"
+env_name = "GrowSpaceSpotlight-MnistMix-v0"
+# env_name = "GrowSpaceEnv-Fairness-v0"
 log_dir = "/tmp/gym/"
 save_dir = "./trained_models/"
 use_proper_time_limits = False
@@ -47,8 +55,8 @@ momentum = 0.9  # if sgd is used
 
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
-    "",
+    "mila",
     sweep_yaml="",
-    proc_num=10,
+    proc_num=1,
     wandb_kwargs={"entity": "growspace"}
 )
