459c4656074069fc442cd69c0dd5c55641168ba8
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-23 17:23:45 +0200

    [CLUSTER] delete

--- config.py
+++ config.py
@@ -13,28 +13,31 @@ gail = False
 gail_experts_dir = './gail_experts'
 gail_batch_size = 128
 gail_epoch = 5
-lr = 2.5e-4
-eps = 1e-5
+lr = 0.04747
+eps = 0.003255
 alpha = 0.99
-gamma = 0.99
-use_gae = True
-gae_lambda = 0.95
-entropy_coef = 0.01
-value_loss_coef = 0.5
-max_grad_norm = 0.5
+gamma = 0.2597
+use_gae = False
+gae_lambda = 0.3391
+entropy_coef = 0.3466
+value_loss_coef = 0.3693
+max_grad_norm = 0.1471
 seed = 1
 cuda_deterministic = False
 num_processes = 1
-num_steps = 2500
+num_steps = 3463
 custom_gym = "growspace"
-ppo_epoch = 4
-num_mini_batch = 32
-clip_param = 0.1
+ppo_epoch = 15
+num_mini_batch = 46
+clip_param = 0.4327
 log_interval = 10
 save_interval = 100
 eval_interval = None
 num_env_steps = 1e6
-env_name = "GrowSpaceEnv-Control-v0" # "GrowSpaceEnv-Continuous-v0"
+env_name = "GrowSpaceEnv-Control-v0"
+# env_name = "GrowSpaceEnv-Hierarchy-v0"
+# env_name = "GrowSpaceSpotlight-MnistMix-v0"
+# env_name = "GrowSpaceEnv-Fairness-v0"
 log_dir = "/tmp/gym/"
 save_dir = "./trained_models/"
 use_proper_time_limits = False
@@ -47,8 +50,8 @@ momentum = 0.9  # if sgd is used
 
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
-    "",
+    "mila",
     sweep_yaml="",
-    proc_num=10,
+    proc_num=1,
     wandb_kwargs={"entity": "growspace"}
 )
