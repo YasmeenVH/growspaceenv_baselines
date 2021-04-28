5b48c047e014e32f6f0bf4fbcf4f9f54f15ca4c1
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2021-04-27 23:26:11 -0400

    [CLUSTER] mnist9_run1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import torch
-
+import numpy as np
 import experiment_buddy
 """ 
 old values of growspace 
@@ -17,28 +17,40 @@ gail = False   # not important
 gail_experts_dir = './gail_experts'  # not important
 gail_batch_size = 128                # not important
 gail_epoch = 5                       # not important
-lr = 2.5e-4
-eps = 1e-5
+#lr = 2.5e-4
+lr = 0.006697
+#eps = 1e-5
+eps = 0.03068
 alpha = 0.99                         # for a2c not ppo
-gamma = 0.99
-use_gae = True
-gae_lambda = 0.95
-entropy_coef = 0.01
-value_loss_coef = 0.5
-max_grad_norm = 0.5
-seed = 1    # didnt change
+#gamma = 0.99
+gamma = 0.8964
+#use_gae = True
+use_gae = False
+#gae_lambda = 0.95
+gae_lambda = 0.3429
+#entropy_coef = 0.01
+entropy_coef = 0.1316
+#value_loss_coef = 0.5
+value_loss_coef = 0.3638
+#max_grad_norm = 0.5
+max_grad_norm = 0.3406
+seed = np.random.randint(1,80, size = 1)   # didnt change
 cuda_deterministic = False
 num_processes = 1
-num_steps = 2500
+#num_steps = 2500
+num_steps = 4240
 custom_gym = "growspace"
-ppo_epoch = 4
-num_mini_batch = 32
-clip_param = 0.1
+#ppo_epoch = 4
+ppo_epoch = 15
+#num_mini_batch = 32
+num_mini_batch = 25
+#clip_param = 0.1
+clip_param = 0.3758
 log_interval = 10  # amount of times we save to wandb
 save_interval = 100 # amount of times we save internal
 eval_interval = None
 num_env_steps = 1e6  # no change
-env_name = "GrowSpaceEnv-Control-v0"#"GrowSpaceSpotlight-Mnist4-v0"
+env_name = "GrowSpaceSpotlight-Mnist9-v0"#"GrowSpaceSpotlight-Mnist4-v0"
 log_dir = "/tmp/gym/"
 save_dir = "./trained_models/"
 use_proper_time_limits = False
@@ -53,7 +65,7 @@ momentum = 0.95
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
     "mila",
-    sweep_yaml="pposweep.yaml",
-    proc_num=10,
+    sweep_yaml="",
+    proc_num=3,
     wandb_kwargs={"entity": "growspace"}
 )
