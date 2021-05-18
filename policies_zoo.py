3d953165413631ac2405c9738837f37a9f9ce5ea
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2021-05-18 16:16:54 -0400

    [CLUSTER] logformanu_again

--- policies_zoo.py
+++ policies_zoo.py
@@ -4,7 +4,7 @@ import torch
 import torch.backends.cudnn
 import torch.utils.data
 import wandb
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.env_util import make_vec_env
 
 import config
@@ -22,6 +22,19 @@ def ppo_stable_baselines_training():
 
     model = PPO("CnnPolicy", envs, verbose=1, tensorboard_log="./runs/", clip_range=0.2, n_steps=50)
     model.learn(total_timesteps=2000, log_interval=1, callback=WandbStableBaselines3Callback())
+    model.save(f"{config.env_name}_stable_baselines_ppo")
+
+def dqn_stable_baselines_training():
+    wandb.run = config.tensorboard.run
+    wandb.tensorboard.patch(save=False, tensorboardX=True)
+
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
+
+    envs = make_vec_env(config.env_name, n_envs=4)
+
+    model = DQN("CnnPolicy", envs, verbose=1, tensorboard_log="./runs/", clip_range=0.2, n_steps=50)
+    model.learn(total_timesteps=1000000, log_interval=1, callback=WandbStableBaselines3Callback())
     model.save(f"{config.env_name}_stable_baselines_ppo")
 
 
